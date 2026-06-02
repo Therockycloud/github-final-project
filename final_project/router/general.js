cbd3670cc949b2d0bf76222ac932a8d75f305203
Therockycloud <157861198+Therockycloud@users.noreply.github.com>
2026-06-02 09:53:51 +0700

    Make Axios usage explicit in book routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,10 +5,9 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-const getBooks = async () => {
+const booksUrl = () => {
   const port = process.env.PORT || 5000;
-  const response = await axios.get(`http://localhost:${port}/books-data`);
-  return response.data;
+  return `http://localhost:${port}/books-data`;
 };
 
 public_users.post("/register", (req,res) => {
@@ -33,7 +32,8 @@ public_users.get('/books-data', function (req, res) {
 // Get the book list available in the shop
 public_users.get('/', async function (req, res) {
   try {
-    return res.status(200).json(await getBooks());
+    const response = await axios.get(booksUrl());
+    return res.status(200).json(response.data);
   } catch (error) {
     return res.status(500).json({message: "Unable to retrieve books."});
   }
@@ -42,7 +42,8 @@ public_users.get('/', async function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', async function (req, res) {
   try {
-    const allBooks = await getBooks();
+    const response = await axios.get(booksUrl());
+    const allBooks = response.data;
     const book = allBooks[req.params.isbn];
 
     if (!book) {
@@ -58,7 +59,8 @@ public_users.get('/isbn/:isbn', async function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author', async function (req, res) {
   try {
-    const allBooks = await getBooks();
+    const response = await axios.get(booksUrl());
+    const allBooks = response.data;
     const author = req.params.author.toLowerCase();
     const matchedBooks = Object.fromEntries(
       Object.entries(allBooks).filter(([, book]) => book.author.toLowerCase().includes(author))
@@ -77,7 +79,8 @@ public_users.get('/author/:author', async function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title', async function (req, res) {
   try {
-    const allBooks = await getBooks();
+    const response = await axios.get(booksUrl());
+    const allBooks = response.data;
     const title = req.params.title.toLowerCase();
     const matchedBooks = Object.fromEntries(
       Object.entries(allBooks).filter(([, book]) => book.title.toLowerCase().includes(title))
@@ -96,7 +99,8 @@ public_users.get('/title/:title', async function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn', async function (req, res) {
   try {
-    const allBooks = await getBooks();
+    const response = await axios.get(booksUrl());
+    const allBooks = response.data;
     const book = allBooks[req.params.isbn];
 
     if (!book) {
